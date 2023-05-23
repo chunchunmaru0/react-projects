ab6d32be65b47503ed0c0250626e0d39730ed59e
chunchunmaru0 <aalupidalu10@gmail.com>
2023-05-22 20:49:52 -0700

    Done?

--- first-project/src/forms.jsx
+++ first-project/src/forms.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import { useState } from 'react';
 
-const Forms = () => {
-    const [name, setName] = useState("");
-    const handleSubmit = event =>{
-        event.preventDefault();
-        alert(`The name you entered was: ${name}`)
-
+function Forms(){
+        const [name, setName] = useState("");
+        const handleSubmit = event =>{
+            event.preventDefault();
+            alert(`The name you entered was: ${name}`)
+    
+        }
+        return (
+            <form onSubmit={ handleSubmit}>
+            <label>Enter your name:
+              <input 
+              type="text" 
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              
+              />
+            </label>
+            <input type="submit" />
+          </form>
+        );
+    
     }
-    return (
-        <form onSubmit={ handleSubmit}>
-        <label>Enter your name:
-          <input 
-          type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          
-          />
-        </label>
-        <input type="submit" />
-      </form>
-    );
-}
+
+
 
 export default Forms;
