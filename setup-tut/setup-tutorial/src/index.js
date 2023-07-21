1eb0095771a91cb74aee878e5a4c6ea166ab556f
chunchunmaru0 <aalupidalu10@gmail.com>
2023-07-20 20:07:05 -0700

    writing component example

--- setup-tut/setup-tutorial/src/index.js
+++ setup-tut/setup-tutorial/src/index.js
@@ -9,10 +9,26 @@ import  ReactDOM  from "react-dom";
 //   return <h1> This is Test with the first Component </h1>
 // }
 
-// arrrow function 
+
+// function Greeting() {
+//   console.log("TYesdt");
+//   return (
+// {/* <div>
+//         <h1>Hello World</h1>
+//       </div>
+// ) */}
+// }
+
+
+// arrrow function is same as the above function
+
 
 const Greeting = () => {
 
-  return React.createElement('h1', {},'hello world')
-}
+  return React.createElement(
+    'div',
+    {},
+    React.createElement('h1', {}, 'hello world Test!')
+  );
+};
 ReactDOM.render(<Greeting/>, document.getElementById('root'));
