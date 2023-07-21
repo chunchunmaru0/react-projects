798ad70bf4ec98845ec3bedecd59d83192ac0a35
chunchunmaru0 <aalupidalu10@gmail.com>
2023-07-20 20:03:41 -0700

    Arrow function

--- setup-tut/setup-tutorial/src/index.js
+++ setup-tut/setup-tutorial/src/index.js
@@ -3,9 +3,16 @@ import  ReactDOM  from "react-dom";
 
 // stateless functional component
 // always return JSX
-function Greeting() {
-  console.log("TYesdt");
-  return <h1> This is Test with the first Component </h1>
-}
 
+// function Greeting() {
+//   console.log("TYesdt");
+//   return <h1> This is Test with the first Component </h1>
+// }
+
+// arrrow function 
+
+const Greeting = () => {
+
+  return React.createElement('h1', {},'hello world')
+}
 ReactDOM.render(<Greeting/>, document.getElementById('root'));
