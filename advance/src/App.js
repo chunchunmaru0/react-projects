68a0db2eb9ee0a5277124d69cc1ecf92a4f5f2ef
chunchunmaru0 <aalupidalu10@gmail.com>
2023-09-15 21:11:37 -0700

    useState example 4

--- advance/src/App.js
+++ advance/src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import ErrorExample from "./tutorial/1-useState/1-error-example";
 import UseStateArray from "./tutorial/1-useState/3-useState-array";
+import UseStateObject from "./tutorial/1-useState/4-useState-object";
 import UseStateBasics from "./tutorial/1-useState/UseStateBasics";
 
 function App() {
   return (
     <div className="container">
-      {/* use state examples */}
-      {/* <ErrorExample />
-      <UseStateBasics /> */}
-      <UseStateArray />
+      {/* use state examples 
+      <ErrorExample />
+      <UseStateBasics />
+      <UseStateArray /> */}
+      <UseStateObject />
     </div>
   );
 }
---
+++ advance/src/tutorial/1-useState/4-useState-object.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const UseStateObject = () => {
+  const [person, setPerson] = useState({
+    name: "peter",
+    age: 24,
+    message: "random message",
+  });
+
+  const changeMessage = () => {
+    //spread operator is must if you wanna retain the previous object value
+    setPerson({ ...person, message: "Hello world" });
+  };
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  );
+};
+
+export default UseStateObject;
